f2e605a1838d6b2e0902c9c0c426dafc1c2b07a2
MalinduDilshan97 <dilshanmalindu.pk@gmail.com>
2018-10-29 23:00:22 +0530

    blah blah

--- New folder/BackEnd/src/main/java/com/javasampleapproach/angular4mongodb/model/Customer.java
+++ New folder/BackEnd/src/main/java/com/javasampleapproach/angular4mongodb/model/Customer.java
@@ -1,47 +1,56 @@
 package com.javasampleapproach.angular4mongodb.model;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "customer")
 public class Customer {
-	@Id
-	private String id;
+    @Id
+    private ObjectId _id;
 
-	private String name;
-	private int age;
+    private String name;
+    private int age;
 
-	public Customer() {
-	}
+    public Customer() {
+    }
 
-	public Customer(String name, int age) {
-		this.name = name;
-		this.age = age;
-	}
+    public Customer(ObjectId _id, String name, int age) {
+        this._id = _id;
+        this.name = name;
+        this.age = age;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public ObjectId get_id() {
+        return _id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void set_id(ObjectId _id) {
+        this._id = _id;
+    }
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public int getAge() {
-		return this.age;
-	}
+    public int getAge() {
+        return age;
+    }
 
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", age=" + age + ", active=" +  "]";
-	}
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "_id=" + _id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
